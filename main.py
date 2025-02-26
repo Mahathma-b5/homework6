5f28c0d062d3d66ed44d8f4c2ca0b7259103c033
Mahathma Reddy <mahathmareddy@Sais-MacBook-Air.local>
2025-02-25 20:08:17 -0500

    updated main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+from app.calculator import Calculator
+from decimal import Decimal, InvalidOperation
+
+from app import App
+
+if __name__ == '__main__':
+    app = App()
+    app.start()
